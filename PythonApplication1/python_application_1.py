952f44671384be8d7aad3ddeb66ed2d01600e1f7
Steven Lowery <31108573+stevenlowery011@users.noreply.github.com>
2018-05-22 16:44:54 -0400

    added docstrings to projects and renamed module to snake_case style

---
+++ PythonApplication1/python_application_1.py
@@ -0,0 +1,4 @@
+"""
+All python files should have a docstring at the top
+"""
+print('hello world!')
--- first-script/first_script.py
+++ first-script/first_script.py
@@ -1,1 +1,4 @@
+"""
+This script prints a basic text to the output
+"""
 print('Congratulations on running this script!!')
