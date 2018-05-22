df01ff7a2368646df2520f1e065f132efb47c3a2
Steven Lowery <31108573+stevenlowery011@users.noreply.github.com>
2018-05-22 16:56:51 -0400

    first udacity scripting practice

--- PythonApplication1/python_application_1.py
+++ PythonApplication1/python_application_1.py
@@ -1,4 +1,20 @@
 """
-All python files should have a docstring at the top
+This module is the sandbox module for python projects
 """
-print('hello world!')
+#print('hello world!')
+how_many_snakes = 1
+snake_string = """
+Welcome to Python3!
+
+             ____
+            / . .\\
+            \  ---<
+             \  /
+   __________/ /
+-=:___________/
+
+<3, Juno
+"""
+
+
+print(snake_string * how_many_snakes)
